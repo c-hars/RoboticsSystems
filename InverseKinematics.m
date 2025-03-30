525394aeae83b5ec574ba1a41c085c9fa74c61e0
semk0vdw <109771706+semk0vdw@users.noreply.github.com>
2025-03-30 16:15:21 +1100

    Update InverseKinematics.m

--- InverseKinematics.m
+++ InverseKinematics.m
@@ -1,18 +1,24 @@
 clear all
+close all
 
-
-l1 = 0.05;
-l2 = 0.2;
-l3 = 0.2;
+% Define link lengths and position
+l1 = 0;
+l2 = 0.4;
+l3 = 0.4;
 l4 = 0.05;
 l5 = 0.05;
-zoffset = 0;
 
-chessboard = 0.3;
-offset = 0.25;
+% Define position and dimensions of chessboard
+chessmidpoint = [0.4; 0; 0];
+chessW = 0.3;
+chessH = 0.02;
 
+% Test positions for validating inverse kinematics
+upperleft_0 = chessmidpoint + [(chessW*3/8); (chessW*3/8); 0.02]
+lowerright_0 = chessmidpoint + [(-chessW*3/8); (-chessW*5/8); 0]
 
-pepos_0 = [0.4; 0.1; -0.2];
+% Position of end effector in zero frame
+pepos_0 = lowerright_0; 
 
 q1 = -atan2(pepos_0(2),pepos_0(1))
 
@@ -24,20 +30,21 @@ p2pos_0 = p3pos_0 + [0;0;(l4)];
 p2pos_1 = R10*p2pos_0 - [l1;0;0];
 
 cos_q3 = (p2pos_1(1)^2 + p2pos_1(3)^2 - (l2^2 + l3^2)) / (2*l2*l3);
-q3 = -acos(max(min(cos_q3, 1), -1)); 
+q3 = -acos(max(min(cos_q3, 1), -1))
 
-q2 = atan2(p2pos_1(3),p2pos_1(1)) - asin(l3*sin(pi-q3)/(sqrt(p2pos_1(1)^2 + p2pos_1(3)^2)))
+q2 = (atan2(p2pos_1(3),p2pos_1(1)) - asin(l3*sin(pi-q3)/(sqrt(p2pos_1(1)^2 + p2pos_1(3)^2))))
 
 q5 = -q1
 
 q4 = -q2 - q3 - pi/2
 
-T10 = transformation_matrix(0,     0, zoffset, q1);
-T21 = transformation_matrix(pi/2,  l1,  0, q2);
-T32 = transformation_matrix(0,    l2,  0, q3);
-T43 = transformation_matrix(0,    l3,  0, q4 + pi/2);
-T54 = transformation_matrix(pi/2, 0, l4, q5);
-T65 = transformation_matrix(0,     0, l5,  0);
+% Transformation matrices of arm
+T10 = transformation_matrix(0,      0,    0,        q1);
+T21 = transformation_matrix(pi/2,  l1,          0,        q2);
+T32 = transformation_matrix(0,     l2,          0,        q3);
+T43 = transformation_matrix(0,     l3,          0, q4 + pi/2);
+T54 = transformation_matrix(pi/2,   0,         l4,        q5);
+T65 = transformation_matrix(0,      0,         l5,         0);
 
 T20 = T21*T10;
 T30 = T32*T20;
@@ -54,26 +61,18 @@ points(:,7) = double(transpose(T60(1:3,1:3))*T60(1:3,4));
 
 disp(points)
 
-
 figure
-scatter3(pepos_0(1), pepos_0(2), pepos_0(3), 'k')
+scatter3(points(1,:),points(2,:),points(3,:), 60, 'filled', 'k')
 hold on
-scatter3(p2pos_0(1), p2pos_0(2), p2pos_0(3), 'k')
 
 for i = 1:6
-   plot3([points(1,i), points(1,i+1)], [points(2,i), points(2,i+1)], [points(3,i), points(3,i+1)], 'r-', 'LineWidth', 2);
-   hold on
+   plot3([points(1,i), points(1,i+1)], [points(2,i), points(2,i+1)], [points(3,i), points(3,i+1)], 'k-', 'LineWidth', 2);
 end
+plot_chessboard(chessmidpoint, chessW, chessW, chessH)
 axis equal
-%xlim([-0.1 1]);
-%ylim([-0.3 0.3]);
-%zlim([0 5]);
 xlabel('X-axis');
 ylabel('Y-axis');
 zlabel('Z-axis');
-
-
-
 
 %% Functions
 
